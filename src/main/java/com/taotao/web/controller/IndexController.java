bbb0756840a0ce1640d6a7a78e6aa2d12b64ba09
lanjunjun <2309936649@qq.com>
2017-04-06 22:46:15 +0800

    大广告位

--- src/main/java/com/taotao/web/controller/IndexController.java
+++ src/main/java/com/taotao/web/controller/IndexController.java
@@ -1,14 +1,20 @@
 package com.taotao.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.taotao.web.service.IndexService;
+
 @Controller
 @RequestMapping("index")
 public class IndexController {
 
+	@Autowired
+	IndexService indexService;
+	
 	/**
 	 * 跳转到首页
 	 * @return
@@ -16,6 +22,7 @@ public class IndexController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView toIndex(){
 		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("AD1", this.indexService.getAD1());
 		return mv;
 	}
 }
